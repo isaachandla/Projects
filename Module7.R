f7651691a6afe718746573b845f128b24a11cd3f
isaachandla <iwchandler5@gmail.com>
2019-09-19 13:04:20 -0500

    7

--- Module7.R
+++ Module7.R
@@ -42,3 +42,26 @@ v[1:15]
 v[c(2, 4, 6, 8, 10)]
 v <- 101:200
 v[seq(from = 1, to = 100, by = 2)]
+m <- matrix(data = 1:80, nrow = 8, ncol = 10, byrow = FALSE)
+m
+x<- m(4,5)
+x <- m[4, 5]
+x
+#Overwriting elements
+##You can replace elements in a vector or matrix, or even entire rows or columns, by identifying the elements to be replaced and then assigning them new values.
+
+m[7, 1] <- 564
+m[, 8] <- 2
+m[2:5, 4:8] <- 1
+m[2:5, 4:8] <- c(20, 19, 18, 17)
+m[2:5, 4:8] <- matrix(data = c(20:1), nrow = 4, ncol = 5, byrow = TRUE)
+m[, 8] <- c("a", "b")
+m
+
+#Lists and dataframes
+##A single list, for example, could contain a matrix, vector of character strings, vector of factors, an array, even another list.
+s <- c("this", "is", "a", "vector", "of", "strings")  # this is a vector of character string
+m <- matrix(data = 1:40, nrow = 5, ncol = 8)  # this is a matrix
+b <- FALSE  # this is a boolean variable
+l <- list(s, m, b)
+l
